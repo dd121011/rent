fadd01e22695151cbb5f2b42bf18583d8c38521b
benny_mouyi@163.com <mouyi265837>
2018-06-03 22:51:43 +0800

    BaseEntity

--- src/main/java/com/scrats/rent/base/entity/BaseEntity.java
+++ src/main/java/com/scrats/rent/base/entity/BaseEntity.java
@@ -3,6 +3,7 @@ package com.scrats.rent.base.entity;
 import lombok.Data;
 
 import javax.persistence.Column;
+import javax.persistence.Transient;
 import java.io.Serializable;
 
 @Data
@@ -11,14 +12,19 @@ public class BaseEntity implements Serializable {
     private static final long serialVersionUID = -4970953425736091134L;
 
     //    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
-    @Column(name = "create_ts")
+    @Column
     private long createTs;//创建时间
 //    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
-    @Column(name = "update_ts")
+    @Column
     private long updateTs;//更新时间
     private String remark;//备注
-//    @Column(name = "delete_ts")
+    @Column
     private long deleteTs;//删除时间戳
 
+    @Transient
+    private Integer page = 1;
+
+    @Transient
+    private Integer rows = 10;
 
 }

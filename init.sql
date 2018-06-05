1b1e8b0aa48e616ebebff8d2addcbb4fcbbde8a1
benny_mouyi@163.com <mouyi265837>
2018-06-05 20:26:05 +0800

    格式

--- init.sql
+++ init.sql
@@ -4,11 +4,11 @@ INSERT INTO `account`(account_id, username, pwd, phone, remark, create_ts) VALUE
 INSERT INTO `account`(account_id, username, pwd, phone, remark, create_ts) VALUES (3, '123123', '111111', '13332941385', '', unix_timestamp(now()));
 
 -- user initial
---超级管理员
+-- 超级管理员
 INSERT INTO `user` (user_id, type, name, sex, age, account_id, create_ts) VALUES (1, '4', 'administrator', '0', '18', 1, unix_timestamp(now()));
---管理员
+-- 管理员
 INSERT INTO `user` (user_id, type, name, sex, age, account_id, create_ts) VALUES (2, '2', 'scrat', '2', '18', 2, unix_timestamp(now()));
---房东
+-- 房东
 INSERT INTO `user` (user_id, type, name, sex, age, account_id, create_ts) VALUES (3, '1', 'ryan', '1', '18', 3, unix_timestamp(now()));
 
 -- dictionary initial

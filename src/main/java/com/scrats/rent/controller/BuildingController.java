df08e4b396d85934e61ee81f675d221a4137b205
benny_mouyi@163.com <mouyi265837>
2018-06-08 00:00:12 +0800

    取消测试代码

--- src/main/java/com/scrats/rent/controller/BuildingController.java
+++ src/main/java/com/scrats/rent/controller/BuildingController.java
@@ -5,6 +5,7 @@ import com.scrats.rent.base.entity.BaseEntity;
 import com.scrats.rent.common.JsonResult;
 import com.scrats.rent.common.PageInfo;
 import com.scrats.rent.entity.Building;
+import com.scrats.rent.entity.User;
 import com.scrats.rent.service.BuildingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -41,9 +42,9 @@ public class BuildingController {
     @ResponseBody
     public String list(BaseEntity base, HttpServletRequest request) {
 
-//        User user = (User) request.getSession().getAttribute("user");
-        int userId = 3;
-        PageInfo<Building> pageInfo = buildingService.getBuildingListByUserId(base.getPage(), base.getRows(), userId);
+        User user = (User) request.getSession().getAttribute("user");
+//        int userId = 3;
+        PageInfo<Building> pageInfo = buildingService.getBuildingListByUserId(base.getPage(), base.getRows(), user.getUserId());
 
         return JSON.toJSONString(new JsonResult(pageInfo.getList(), (int) pageInfo.getTotal()));
     }

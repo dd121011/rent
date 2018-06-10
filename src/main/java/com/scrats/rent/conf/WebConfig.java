f54c8479e4239faab84fb6576324aecd85d9ce3c
Ryan <benny_mouyi@163.com>
2018-06-10 19:27:22 +0800

    修复返回值解析错误

--- src/main/java/com/scrats/rent/conf/WebConfig.java
+++ src/main/java/com/scrats/rent/conf/WebConfig.java
@@ -4,9 +4,9 @@ import com.scrats.rent.common.APIRequestResolver;
 import com.scrats.rent.common.interceptor.AuthenticationInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
@@ -50,37 +50,26 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         super.addArgumentResolvers(argumentResolvers);
     }
 
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(fastJsonHttpMessageConverterEx());
-        super.configureMessageConverters(converters);
-    }
-
-    @Bean
-    public FastJsonHttpMessageConverterEx fastJsonHttpMessageConverterEx() {
-        return new FastJsonHttpMessageConverterEx();
-    }
-
-
-    /**
-     * 配置静态访问资源
-     * @param registry
-     */
 //    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
-////        registry.addResourceHandler("/my/**").addResourceLocations("file:E:/my/");
-//        super.addResourceHandlers(registry);
+//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        converters.add(fastJsonHttpMessageConverterEx());
+//        super.configureMessageConverters(converters);
 //    }
+
+//    @Bean
+//    public FastJsonHttpMessageConverterEx fastJsonHttpMessageConverterEx() {
+//        return new FastJsonHttpMessageConverterEx();
+//    }
+
 
     /**
      * 以前要访问一个页面需要先创建个Controller控制类，再写方法跳转到页面
      * 在这里配置后就不需要那么麻烦了,直接访问http://localhost:8080/toLogin就跳转到login.jsp页面了
      * @param registry
      */
-//    @Override
-//    public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addViewController("/").setViewName("login");
-//        super.addViewControllers(registry);
-//    }
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("login");
+        super.addViewControllers(registry);
+    }
 }
--- src/main/java/com/scrats/rent/controller/LoginController.java
+++ src/main/java/com/scrats/rent/controller/LoginController.java
@@ -7,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -27,13 +26,6 @@ public class LoginController {
 
     @Autowired
     private AccountService accountService;
-
-    @IgnoreSecurity
-    @GetMapping("/")
-    public String gologin() {
-
-        return "login";
-    }
 
     @IgnoreSecurity
     @PostMapping("/login")

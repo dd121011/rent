241a649a0a7a986369e6383e14399e320eef1c57
Ryan <benny_mouyi@163.com>
2018-06-14 00:32:00 +0800

    获取全部配套设施、获取全部额外收费项、编辑房子接口done

--- src/main/java/com/scrats/rent/api/BuildingApi.java
+++ src/main/java/com/scrats/rent/api/BuildingApi.java
@@ -1,13 +1,17 @@
 package com.scrats.rent.api;
 
 import com.alibaba.fastjson.JSON;
+import com.scrats.rent.base.service.RedisService;
 import com.scrats.rent.common.APIRequest;
 import com.scrats.rent.common.JsonResult;
 import com.scrats.rent.common.PageInfo;
 import com.scrats.rent.common.annotation.APIRequestControl;
+import com.scrats.rent.constant.GlobalConst;
 import com.scrats.rent.entity.Building;
-import com.scrats.rent.service.BuildingService;
-import com.scrats.rent.service.UserService;
+import com.scrats.rent.entity.BuildingLandlord;
+import com.scrats.rent.entity.DictionaryIterm;
+import com.scrats.rent.entity.Extra;
+import com.scrats.rent.service.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,7 +37,19 @@ public class BuildingApi {
     private BuildingService buildingService;
 
     @Autowired
-    private UserService userService;
+    private RedisService redisService;
+
+    @Autowired
+    private DictionaryService dictionaryService;
+
+    @Autowired
+    private DictionaryItermService dictionaryItermService;
+
+    @Autowired
+    private ExtraService extraService;
+
+    @Autowired
+    private BuildingLandlordService buildingLandlordService;
 
 
     @PostMapping("/list")
@@ -41,5 +57,32 @@ public class BuildingApi {
         PageInfo<Building> pageInfo = buildingService.getBuildingListByUserId(apiRequest.getPage(), apiRequest.getRows(), apiRequest.getUser().getUserId());
 
         return JSON.toJSONString(new JsonResult<List>(pageInfo.getList(), (int) pageInfo.getTotal()));
+    }
+
+    @PostMapping("/extrasAll")
+    public String extrasAll() {
+        List<Extra> extras = extraService.selectAll();
+        return JSON.toJSONString(new JsonResult<List>(extras));
+    }
+
+    @PostMapping("/facilitiestAll")
+    public String extras() {
+        List<DictionaryIterm> facilities = dictionaryItermService.getDicItermByDicCode(GlobalConst.FACILITY_CODE);
+        return JSON.toJSONString(new JsonResult<List>(facilities));
+    }
+
+    @PostMapping("/edit")
+    public String edit(@APIRequestControl APIRequest apiRequest, Building building) {
+
+        if(null != building.getBuildingId()){
+            buildingService.updateByPrimaryKeySelective(building);
+        }else{
+            building.setCreateTs(System.currentTimeMillis());
+            buildingService.insertSelective(building);
+            BuildingLandlord buildingLandlord = new BuildingLandlord(apiRequest.getUser().getUserId(), building.getBuildingId());
+            buildingLandlordService.insertSelective(buildingLandlord);
+        }
+
+        return JSON.toJSONString(new JsonResult<>());
     }
 }

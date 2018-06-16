4e9d0b29044a274661a3d5a7117bc3fcd0642b72
huzhenjie <huzhenjie.dev@gmail.com>
2018-06-16 23:59:36 +0800

    push target jar file into project dir

--- release.sh
+++ release.sh
@@ -8,4 +8,4 @@ mvn clean package
 sed 's/active: release/active: dev/g' src/main/resources/application.yml > src/main/resources/.application.yml.tmp
 mv src/main/resources/.application.yml.tmp src/main/resources/application.yml
 
-scp target/rent.jar root@47.106.212.7:/root/
+scp target/rent.jar root@47.106.212.7:/root/project/Rent/

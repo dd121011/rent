99924002d51c4b65aa2a0b4b41158a8ffcf393e4
Ryan <benny_mouyi@163.com>
2018-06-24 10:40:22 +0800

    增加roomRenter相关

---
+++ src/main/java/com/scrats/rent/service/RoomRenterService.java
@@ -0,0 +1,16 @@
+package com.scrats.rent.service;
+
+import com.scrats.rent.base.service.BaseService;
+import com.scrats.rent.entity.RoomRenter;
+import com.scrats.rent.mapper.RoomRenterMapper;
+
+/**
+ * Created with scrat.
+ * Description: ${DESCRIPTION}.
+ * Email:    guosq@scrats.cn.
+ * Author:   lol.
+ * Date:     2018/6/6 22:33.
+ */
+public interface RoomRenterService extends BaseService<RoomRenter, RoomRenterMapper> {
+
+}

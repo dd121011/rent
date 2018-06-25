dfb7b2c695fe53638356ea6ef0b19b5f0f4973be
Ryan <benny_mouyi@163.com>
2018-06-25 11:18:54 +0800

    修改表结构

--- rent.sql
+++ rent.sql
@@ -201,8 +201,8 @@ CREATE TABLE `room` (
     `living` int(2) unsigned NOT NULL COMMENT '客厅数量',
     `bedroom` int(2) unsigned NOT NULL COMMENT '房间数量',
     `toilet` int(2) unsigned NOT NULL COMMENT '卫生间数量',
-    `orientation` varchar(32) DEFAULT '' COMMENT '房间朝向',
-    `decoration` varchar(32) DEFAULT '' COMMENT '装修情况',
+    `orientation` char(4) NOT NULL COMMENT '房间朝向',
+    `decoration` char(4) NOT NULL COMMENT '装修情况',
     `guaranty` int(2) unsigned NOT NULL COMMENT '押金月份',
     `pay` int(2) unsigned NOT NULL COMMENT '租金月份',
     `rent_fee` int(10) unsigned NOT NULL COMMENT '租金[分/月]',
@@ -210,12 +210,13 @@ CREATE TABLE `room` (
     `description` varchar(256) DEFAULT '' COMMENT '描述',
     `facilities` varchar(256) DEFAULT '' COMMENT '配套设施id字符串[,隔开]',
     `extra_fee` varchar(256) DEFAULT '' COMMENT '额外收费项id字符串[,隔开]',
-    `rent_ts` bigint unsigned DEFAULT '0' COMMENT '出租时间',
+    `rent_ts` bigint unsigned NOT NULL DEFAULT '0' COMMENT '出租时间',
     `building_id` int(10) unsigned NOT NULL COMMENT '房子id,一个房间对应一个房子id',
     `remark` varchar(256) DEFAULT '' COMMENT '备注',
     `create_ts` bigint unsigned NOT NULL COMMENT '创建时间，13位时间戳',
     `update_ts` bigint unsigned NOT NULL DEFAULT '0' COMMENT '更新时间, 13位时间戳',
     `delete_ts` bigint unsigned NOT NULL DEFAULT '0' COMMENT '删除时间, 13位时间戳',
+    UNIQUE KEY (`room_no`,`building_id`),
     PRIMARY KEY (`room_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -256,22 +257,22 @@ CREATE TABLE `room_renter` (
 -- ----------------------------
 DROP TABLE IF EXISTS `bargin`;
 CREATE TABLE `bargin` (
-    `bargin_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `bargin_no` char(32) NOT NULL COMMENT '合同编号',
     `name` varchar(64) NOT NULL COMMENT '姓名',
     `sex` char(1) NOT NULL DEFAULT '0' COMMENT '性別, 0-保密, 1-男, 2-女',
     `phone` char(11) NOT NULL COMMENT '手机号码',
     `id_card` varchar(18) NOT NULL COMMENT 'identification card 身份证号',
-    `id_card_pic` varchar(64) NOT NULL DEFAULT '' COMMENT '身份证正面',
-    `id_card_pic_back` varchar(64) NOT NULL DEFAULT '' COMMENT '身份证反面',
-    `guaranty` int(2) DEFAULT 1 COMMENT '押金月份',
-    `pay` int(2) DEFAULT 1 COMMENT '租金月份',
+    `id_card_pic` varchar(64) DEFAULT '' COMMENT '身份证正面',
+    `id_card_pic_back` varchar(64) DEFAULT '' COMMENT '身份证反面',
+    `guaranty` int(2) unsigned NOT NULL DEFAULT '0' COMMENT '押金月份',
+    `pay` int(2) unsigned NOT NULL DEFAULT '0' COMMENT '租金月份',
     `rent_fee` int(10) unsigned NOT NULL COMMENT '租金[分/月]',
     `guaranty_fee` int(10) unsigned NOT NULL COMMENT '押金[分]',
     `total` int(10) unsigned NOT NULL COMMENT '首次缴费[分]',
-    `water` int(10) unsigned DEFAULT '0' COMMENT '水表初始读数, 单位KG',
-    `electric` int(10) unsigned DEFAULT '0' COMMENT '电表初始读数, 单位Kwh',
-    `electric_three` int(10) unsigned DEFAULT '0' COMMENT '三相电表初始读数, 单位Kwh',
-    `gas` int(10) unsigned DEFAULT '0' COMMENT '天然气初始读数, 单位m2',
+    `water` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '水表初始读数, 单位KG',
+    `electric` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '电表初始读数, 单位Kwh',
+    `electric_three` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '三相电表初始读数, 单位Kwh',
+    `gas` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '天然气初始读数, 单位m2',
     `facilities` varchar(256) DEFAULT '' COMMENT '配套设施id字符串[,隔开]',
     `room_id` int(10) unsigned NOT NULL COMMENT '房间id,一个合同对应一个房间，一个房间对应多个合同',
     `building_id` int(10) unsigned NOT NULL COMMENT '房子Id',
@@ -283,7 +284,7 @@ CREATE TABLE `bargin` (
     `create_ts` bigint unsigned NOT NULL COMMENT '创建时间，13位时间戳',
     `update_ts` bigint unsigned NOT NULL DEFAULT '0' COMMENT '更新时间, 13位时间戳',
     `delete_ts` bigint unsigned NOT NULL DEFAULT '0' COMMENT '删除时间, 13位时间戳',
-    PRIMARY KEY (`bargin_id`)
+    PRIMARY KEY (`bargin_no`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -296,7 +297,7 @@ CREATE TABLE `bargin_extra` (
     `room_id` int(10) unsigned NOT NULL COMMENT '房间Id',
     `extra_id` int(10) unsigned NOT NULL COMMENT '额外收费项Id',
     `name` varchar(10) NOT NULL COMMENT '名称',
-    `unit` varchar(10) NOT NULL COMMENT '单位',
+    `unit` varchar(10) DEFAULT '' COMMENT '单位',
     `price` int(10) unsigned NOT NULL COMMENT '价格',
     `remark` varchar(256) DEFAULT '' COMMENT '备注',
     `create_ts` bigint unsigned NOT NULL COMMENT '创建时间，13位时间戳',
@@ -310,19 +311,19 @@ CREATE TABLE `bargin_extra` (
 -- ----------------------------
 DROP TABLE IF EXISTS `deposit`;
 CREATE TABLE `deposit` (
-    `deposit_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `deposit_no` char(32) NOT NULL COMMENT '押金编号',
     `room_no` varchar(10) NOT NULL COMMENT '房间编号',
     `fee` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '总费用',
     `pay_ts` bigint unsigned NOT NULL DEFAULT '0' COMMENT '支付时间戳，13位',
     `pay_no` varchar(256) DEFAULT '' COMMENT '支付订单号',
-    `channel` char(1) DEFAULT NULL COMMENT '支付渠道，0-线下支付；1-微信支付；2-支付宝支付',
+    `channel` char(2) DEFAULT '99' COMMENT '支付渠道，99-未支付; 0-线下支付; 1-微信支付; 2-支付宝支付',
     `room_id` int(10) unsigned NOT NULL COMMENT '房间id,一个押金对应一个roomId,一个roomId可能对应多个押金Id',
     `renter_id` int(10) unsigned NOT NULL COMMENT '一个押金对应一个租客',
     `remark` varchar(256) DEFAULT '' COMMENT '备注',
     `create_ts` bigint unsigned NOT NULL COMMENT '创建时间，13位时间戳',
     `update_ts` bigint unsigned NOT NULL DEFAULT '0' COMMENT '更新时间, 13位时间戳',
     `delete_ts` bigint unsigned NOT NULL DEFAULT '0' COMMENT '删除时间, 13位时间戳',
-    PRIMARY KEY (`deposit_id`)
+    PRIMARY KEY (`deposit_no`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -336,7 +337,7 @@ CREATE TABLE `deposit_iterm` (
     `building_id` int(10) unsigned NOT NULL COMMENT '房子Id',
     `name` varchar(32) NOT NULL COMMENT '押金项目名称',
     `price` int(10) NOT NULL COMMENT '押金项目单价[分]',
-    `unit` varchar(10) NOT NULL COMMENT '押金项目单位',
+    `unit` varchar(10) DEFAULT '' COMMENT '押金项目单位',
     `number` int(10) unsigned NOT NULL COMMENT '押金项目数量',
     `money` int(10) unsigned NOT NULL COMMENT '押金项目金额',
     `remark` varchar(256) DEFAULT '' COMMENT '备注',
@@ -352,14 +353,14 @@ CREATE TABLE `deposit_iterm` (
 DROP TABLE IF EXISTS `rent`;
 CREATE TABLE `rent` (
     `rent_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-    `rent_no` varchar(64) NOT NULL COMMENT '房租收据单号',
+    `rent_no` varchar(32) NOT NULL COMMENT '房租收据单号',
     `rent_month` char(6) NOT NULL COMMENT '房租月份, eg 201806',
-    `fee` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '总费用',
-    `count` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '折扣费用',
-    `real_fee` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '实际费用',
+    `fee` int(10) unsigned NOT NULL COMMENT '总费用',
+    `count` int(10) unsigned NOT NULL default '0' COMMENT '折扣费用',
+    `real_fee` int(10) unsigned NOT NULL COMMENT '实际费用',
     `pay_ts` bigint unsigned NOT NULL DEFAULT '0' COMMENT '支付时间戳，13位',
     `pay_no` varchar(256) DEFAULT '' COMMENT '支付订单号',
-    `channel` char(1) DEFAULT NULL COMMENT '支付渠道，0-线下支付；1-微信支付；2-支付宝支付',
+    `channel` char(2) DEFAULT '99' COMMENT '支付渠道，0-线下支付；1-微信支付；2-支付宝支付',
     `room_id` int(10) unsigned NOT NULL COMMENT '房间id,一个房租对应一个roomId',
     `remark` varchar(256) DEFAULT '' COMMENT '备注',
     `create_ts` bigint unsigned NOT NULL COMMENT '创建时间，13位时间戳',
@@ -379,7 +380,7 @@ CREATE TABLE `rent_iterm` (
     `building_id` int(10) unsigned NOT NULL COMMENT '房子Id',
     `name` varchar(32) NOT NULL COMMENT '房租项目名称',
     `price` int(10) unsigned NOT NULL COMMENT '房租项目单价[分]',
-    `unit` varchar(32) NOT NULL COMMENT '房租项目单位,从合同获得',
+    `unit` varchar(32) DEFAULT '' COMMENT '房租项目单位,从合同获得',
     `number` int(10) unsigned NOT NULL COMMENT '房租项目数量',
     `money` int(10) unsigned NOT NULL COMMENT '房租项目金额',
     `description` varchar (256) DEFAULT '' COMMENT '房租项目描述',
@@ -398,7 +399,7 @@ CREATE TABLE `water_history` (
     `water_history_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
     `room_id` varchar(10) NOT NULL COMMENT '房间Id',
     `building_id` int(10) unsigned NOT NULL COMMENT '房子Id',
-    `count` int(10) unsigned DEFAULT '0' COMMENT '水表读数, 单位KG',
+    `count` int(10) unsigned NOT NULL COMMENT '水表读数, 单位KG',
     `month` char(6) NOT NULL COMMENT '统计月, eg 201805',
     `remark` varchar(256) DEFAULT '' COMMENT '备注',
     `create_ts` bigint unsigned NOT NULL COMMENT '创建时间，13位时间戳',
@@ -415,7 +416,7 @@ CREATE TABLE `electric_history` (
     `electric_history_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
     `room_id` varchar(10) NOT NULL COMMENT '房间Id',
     `building_id` int(10) unsigned NOT NULL COMMENT '房子Id',
-    `count` int(10) unsigned DEFAULT '0' COMMENT '电表读数, 单位Kwh',
+    `count` int(10) unsigned NOT NULL COMMENT '电表读数, 单位Kwh',
     `month` char(6) NOT NULL COMMENT '统计月, eg 201805',
     `remark` varchar(256) DEFAULT '' COMMENT '备注',
     `create_ts` bigint unsigned NOT NULL COMMENT '创建时间，13位时间戳',
@@ -432,7 +433,7 @@ CREATE TABLE `electric_three_history` (
     `electric_three_history_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
     `room_id` varchar(10) NOT NULL COMMENT '房间Id',
     `building_id` int(10) unsigned NOT NULL COMMENT '房子Id',
-    `count` int(10) unsigned DEFAULT '0' COMMENT '三相电表读数, 单位Kwh',
+    `count` int(10) unsigned NOT NULL COMMENT '三相电表读数, 单位Kwh',
     `month` char(6) NOT NULL COMMENT '统计月, eg 201805',
     `remark` varchar(256) DEFAULT '' COMMENT '备注',
     `create_ts` bigint unsigned NOT NULL COMMENT '创建时间，13位时间戳',
@@ -449,7 +450,7 @@ CREATE TABLE `gas_history` (
     `gas_history_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
     `room_id` varchar(10) NOT NULL COMMENT '房间Id',
     `building_id` int(10) unsigned NOT NULL COMMENT '房子Id',
-    `count` int(10) unsigned DEFAULT '0' COMMENT '天然气读数, 单位m2',
+    `count` int(10) unsigned NOT NULL COMMENT '天然气读数, 单位m2',
     `month` char(6) NOT NULL COMMENT '统计月, eg 201805',
     `remark` varchar(256) DEFAULT '' COMMENT '备注',
     `create_ts` bigint unsigned NOT NULL COMMENT '创建时间，13位时间戳',

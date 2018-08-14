f55f71bdb5f605b45ab07a11822fe7313efa824d
huzhenjie <huzhenjie.dev@gmail.com>
2018-08-14 13:52:45 +0800

    java 10 support

--- src/main/java/com/scrats/rent/util/MD5Util.java
+++ src/main/java/com/scrats/rent/util/MD5Util.java
@@ -1,10 +1,6 @@
 package com.scrats.rent.util;
 
-import sun.misc.BASE64Encoder;
-
-import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 /**
  * @Created with scrat.
@@ -15,62 +11,28 @@ import java.security.NoSuchAlgorithmException;
  */
 public class MD5Util {
 
-    /**利用MD5进行加密
-     * @param str  待加密的字符串
-     * @return  加密后的字符串
-     * @throws NoSuchAlgorithmException  没有这种产生消息摘要的算法
-     * @throws UnsupportedEncodingException
-     */
-    public static String EncoderByMd5(String str) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-        //确定计算方法
-        MessageDigest md5=MessageDigest.getInstance("MD5");
-        BASE64Encoder base64en = new BASE64Encoder();
-        //加密后的字符串
-        String newstr=base64en.encode(md5.digest(str.getBytes("utf-8")));
-        return newstr;
-    }
-
-    /**判断用户密码是否正确
-     * @param newpasswd  用户输入的密码
-     * @param oldpasswd  数据库中存储的密码－－用户密码的摘要
-     * @return
-     * @throws NoSuchAlgorithmException
-     * @throws UnsupportedEncodingException
-     */
-    public static boolean checkpassword(String newpasswd,String oldpasswd) throws NoSuchAlgorithmException, UnsupportedEncodingException{
-        if(EncoderByMd5(newpasswd).equals(oldpasswd)){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-
     public static String md5(String str) {
         try {
             MessageDigest md = MessageDigest.getInstance("MD5");
             md.update(str.getBytes());
-            byte b[] = md.digest();
+            byte digests[] = md.digest();
 
-            int i;
-
-            StringBuffer buf = new StringBuffer("");
-            for (int offset = 0; offset < b.length; offset++) {
-                i = b[offset];
-                if (i < 0){
+            StringBuilder sb = new StringBuilder();
+            for (byte b : digests) {
+                int i = b;
+                if (i < 0) {
                     i += 256;
                 }
-                if (i < 16){
-                    buf.append("0");
+                if (i < 16) {
+                    sb.append("0");
                 }
-                buf.append(Integer.toHexString(i));
+                sb.append(Integer.toHexString(i));
             }
-            str = buf.toString();
+            return sb.toString();
         } catch (Exception e) {
             e.printStackTrace();
-
+            return null;
         }
-        return str;
     }
 
 }
